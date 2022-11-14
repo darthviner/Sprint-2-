e9c98e21d5ab860b5901d847fb31357b242331e8
darthviner <darth.viner@yandex.ru>
2022-11-14 21:42:52 +0600

    Added tests

---
+++ src/test/java/utils/Client.java
@@ -0,0 +1,17 @@
+package utils;
+
+import io.restassured.builder.RequestSpecBuilder;
+import io.restassured.http.ContentType;
+import io.restassured.response.ValidatableResponse;
+import io.restassured.specification.RequestSpecification;
+
+public class Client {
+    private static final String BASE_URI = "https://qa-scooter.praktikum-services.ru";
+
+    protected RequestSpecification getSpec(){
+        return new RequestSpecBuilder()
+                .setContentType(ContentType.JSON)
+                .setBaseUri(BASE_URI)
+                .build();
+    }
+}
---
+++ src/test/java/utils/CourierClient.java
@@ -0,0 +1,42 @@
+package utils;
+
+import io.qameta.allure.Step;
+import io.restassured.response.ValidatableResponse;
+import io.restassured.RestAssured;
+import models.Courier;
+import models.Credentials;
+
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.equalTo;
+
+public class CourierClient extends Client{
+    private static String CREATE_PATH = "/api/v1/courier";
+    private static String LOGIN_PATH = "/api/v1/courier/login";
+    private static String DELETE_PATH = "/api/v1/courier/";
+    @Step("Create courier")
+    public ValidatableResponse create(Courier courier){
+        return given()
+                .spec(getSpec())
+                .body(courier)
+                .when()
+                .post(CREATE_PATH).then();
+    }
+    @Step("Delete courier")
+    public ValidatableResponse delete(int id){
+        return  given()
+                .spec(getSpec())
+                .when()
+                .delete(DELETE_PATH+id)
+                .then();
+
+    }
+    @Step("Login courier with credentials")
+    public ValidatableResponse login(Credentials credentials){
+        return given()
+                .spec(getSpec())
+                .body(credentials)
+                .when()
+                .post(LOGIN_PATH)
+                .then();
+    }
+}
